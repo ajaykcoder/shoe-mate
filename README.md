d0c735f8fc091a2d39f6186b6d5373e2409f608d
Ajay <ajaykcoder@gmail.com>
2024-03-22 12:19:42 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Shoe Mate
+
+### Pull the latest changes from the github
+    git pull origin main
+
+### Install node modules
+    npm install
+
+### Run Development Server
+    npm run dev
---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const App = () => {
+    return (
+        <h1 className="text-center text-3xl text-cyan-600 mt-4 font-semibold">Shoe Mate</h1>
+    );
+}
+export default App;
