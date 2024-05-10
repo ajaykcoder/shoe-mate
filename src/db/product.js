3404f474bce85fc2fdbc7b22a0f2ae8bcfbaaaa5
Ajay <ajaykcoder@gmail.com>
2024-05-10 15:42:19 +0530

    changes

--- src/db/product.js
+++ src/db/product.js
@@ -5,6 +5,7 @@ const product = [
         price: "10999",
         isFavorite: 0,
         rating: 1.6,
+        slug: "PRO11020244237",
         images: [
             {picture: "https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/3bbecbdf584e40398446a8bf0117cf62_9366/Samba_OG_Shoes_White_B75806_01_standard.jpg"},
             {picture: "https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/4c70105150234ac4b948a8bf01187e0c_9366/Samba_OG_Shoes_Black_B75807_01_standard.jpg"},
@@ -18,6 +19,7 @@ const product = [
         price: "2399",
         isFavorite: 0,
         rating: 3,
+        slug: "PRO11020267889",
         images: [
             {picture: "https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/acbcd0cf1ce14b59bf36adb900ae5715_9366/ADI-SHAWT_SPORT_INSPIRED_SHOES_Blue_EY3042_01_standard.jpg"},
             {picture: "https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/0fcb323d9a084a5ab3faadba0101f74e_9366/ADI-SHAWT_SPORT_INSPIRED_SHOES_Black_EY3044_01_standard.jpg"},
@@ -30,6 +32,7 @@ const product = [
         price: "2399",
         isFavorite: 0,
         rating: 5,
+        slug: "PRO11020244678",
         images: [
             {picture: "https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/79db381612904d9eafe6ad2801551b98_9366/Adilette_Comfort_Adjustable_Bandage_Slides_Black_GZ8951_01_standard.jpg"},
             {picture: "https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/ee7d05340c5e444695f4afab009b2465_9366/Adilette_Comfort_Adjustable_Bandage_Slides_White_GZ8950_01_standard.jpg"}
@@ -41,6 +44,7 @@ const product = [
         price: "3359",
         isFavorite: 0,
         rating: 4.5,
+        slug: "PRO11020225801",
         images: [
             {picture: "https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/636e13a768c64ec6b9c4ab5000fbbf0e_9366/Ultimashow_Shoes_White_FX3631_01_standard.jpg"},
             {picture: "https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/eb8c0bf24c6649519afcab4a009f4e10_9366/Ultimashow_Shoes_Black_FX3624_01_standard.jpg"},
@@ -53,6 +57,7 @@ const product = [
         price: "6999",
         isFavorite: 0,
         rating: 1.6,
+        slug: "PRO11020202975",
         images: [
             {picture: "https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/664ee84a7a184935bc85adc50168f41a_9366/Hoops_3.0_Mid_Classic_Vintage_Shoes_Black_GW3019_01_standard.jpg"},
             {picture: "https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/ebe01e76794b4d8cbd56adc5016cdbda_9366/Hoops_3.0_Mid_Classic_Vintage_Shoes_Black_GW3020_01_standard.jpg"},
